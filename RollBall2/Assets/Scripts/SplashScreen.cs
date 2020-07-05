d7e6644a505f980af936301f72010d68df0dc558
Faisalse <faisalaslam156@gmail.com>
2020-07-04 23:57:32 -0700

    Final project

---
+++ RollBall2/Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    public static int ScreenNumber;
+    // Start is called before the first frame update
+    
+
+    public void startGame()
+    {
+        if (ScreenNumber ==0)
+        {
+            ScreenNumber = 1;
+            SceneManager.LoadScene(ScreenNumber);
+        }
+       
+    }
+}
